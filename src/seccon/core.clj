9e9d5ffc68b232535db9e02ea83744590ef2ae25
Ketaro Takeuchi <tiyotiyouda@gmail.com>
2014-12-06 12:28:54 +0900

    solved pro2

--- src/seccon/core.clj
+++ src/seccon/core.clj
@@ -1,34 +1,45 @@
 (ns seccon.core
-  (:use [clojure.string :only [split-line split]]))
+  (:use [clojure.string :only [split]]))
+
+(def pro2 "87 101 108 1100011 0157 6d 0145 040 116 0157 100000 0164 104 1100101 32 0123 69 67 0103 1001111 1001110 040 062 060 49 064 100000 0157 110 6c 0151 1101110 101 040 0103 1010100 70 101110 0124 1101000 101 100000 1010011 1000101 67 0103 4f 4e 100000 105 1110011 040 116 1101000 0145 040 1100010 0151 103 103 0145 1110011 0164 100000 1101000 0141 99 6b 1100101 0162 32 0143 111 1101110 1110100 101 0163 0164 040 0151 0156 040 74 0141 1110000 1100001 0156 056 4f 0157 0160 115 44 040 0171 1101111 117 100000 1110111 0141 0156 1110100 32 0164 6f 32 6b 1101110 1101111 1110111 100000 0164 1101000 0145 040 0146 6c 97 1100111 2c 100000 0144 111 110 100111 116 100000 1111001 6f 117 63 0110 1100101 0162 0145 100000 1111001 111 117 100000 97 114 0145 46 1010011 0105 0103 67 79 1001110 123 87 110011 110001 67 110000 1001101 32 55 060 100000 110111 0110 110011 32 53 51 0103 0103 060 0116 040 5a 0117 73 0101 7d 1001000 0141 1110110 1100101 100000 102 0165 0156 33")
 
 (def alphabets 
-  (let [z (concat (->> (range (int \A) (inc (int \Z))))
-                  (->> (range (int \a) (inc (int \a)))))]
+  (let [z (range 32 (inc (int \~)))]
     (zipmap z (map char z))))
 
-(defn x->alphabet [x]
-  (or (16->alphabet x)
-      (2->alphabet x)
-      (8->alphabet x)
-      (10->alphabet x)))
-
-(defn 2->alphabet [x]
+(defn two->alphabet [x]
   (as-> x $
         (Integer/parseInt $ 2) 
         (get alphabets $)))
 
-(defn 8->alphabet [x]
+(defn eight->alphabet [x]
   (as-> x $
+        (drop 1 $)
+        (apply str $)
         (Integer/parseInt $ 8)
-        (get alphabet $)))
+        (get alphabets $)))
 
-(defn 16->alphabet [x]
+(defn sixteen->alphabet [x]
   (as-> x $
-        (Integer/parseInt $ 16)
-        (get alphabet $)))
+        (str "0x" $)
+        (Integer/decode $)
+        (get alphabets $)))
+
+(defn ten->alphabet [x]
+  (as-> x $
+        (Integer/parseInt $)
+        (get alphabets $)))
+
+(defn x->alphabet [x]
+  (let [len (count x)
+        head (-> x first)]
+    (cond (>= len 6) (two->alphabet x)
+          (= head \0) (eight->alphabet x)
+          (some (->> (range (int \a) (inc (int \z))) (map char) set) x) (sixteen->alphabet x)
+          :else (ten->alphabet x))))
 
 (defn solve [s]
   (as-> s $
         (split $ #" ")
-        (x->alphabet $)
-        (reduce str $)))
+        (map x->alphabet $)
+        (apply str $)))
